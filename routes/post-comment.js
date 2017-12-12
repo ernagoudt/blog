9df64d25a142118c8189c532d8db943deaadf762
Erna <erna.goudt@lendahand.com>
2017-12-12 20:19:40 +0100

    styling changes

--- routes/post-comment.js
+++ routes/post-comment.js
@@ -10,11 +10,21 @@ module.exports = (app, client) => {
     FROM users
     WHERE users.id = '${req.session.user.id}'`
   }
+  const query2 = {
+    text: `SELECT users.username, comments.comment, comments.message_id
+    FROM comments
+    LEFT JOIN users
+    ON users.id = comments.user_id`
+  }
 
   client.query(query, (err, result) => {
     if (err) throw err
+      client.query(query2, (err2, result2) => {
+        if (err2) throw err
+        var comments = result2.rows
+      })
 
-  res.redirect("all-messages")
+  res.render("all-messages", {comments:comments})
 
   })
 })
