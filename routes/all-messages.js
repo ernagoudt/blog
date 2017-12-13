382c704a5f406f0c52ec0bd6ebea3dfac9880a6d
Erna <erna.goudt@lendahand.com>
2017-12-13 17:32:28 +0100

    adding comments

--- routes/all-messages.js
+++ routes/all-messages.js
@@ -1,22 +1,41 @@
 module.exports = (app, client) => {
 app.get("/all-messages", (req, res) => {
   const query = {
-    text: `SELECT messages.id, messages.title, messages.body, messages.user_id, users.username
-    FROM messages
-    INNER JOIN users
-    ON messages.user_id = users.id`
+    text: `SELECT messageuser.username AS messageusername,
+    commentusers.username AS commentusername,
+    messageuser.body AS messagebody,
+    messageuser.title AS messagetitle,
+    commentusers.comment AS comment,
+    messageuser.id,
+    commentusers.message_id
+
+    FROM
+
+      ( SELECT users.username, messages.title, messages.body, messages.id
+      FROM messages
+      LEFT JOIN users
+      ON users.id = messages.user_id
+      ) AS messageuser
+
+    LEFT JOIN
+
+      ( SELECT array_agg(users.username) AS username,
+      array_agg(comments.comment) AS comment,
+      comments.message_id
+      FROM comments
+      LEFT JOIN users
+      ON users.id = comments.user_id
+      GROUP BY comments.message_id
+      ) AS commentusers
+
+    ON commentusers.message_id = messageuser.id`
   }
 
   client.query(query, (err, result) => {
     if (err) throw err
     var allMessages = result.rows
-    /*console.log(result.rows)*/
 
-    res.render("all-messages", {
-      title: "Bulletin Board",
-      allMessages:allMessages,
-      user:req.session.user
-    })
+    res.render("all-messages", {allMessages:allMessages, user:req.session.user})
   })
 })
 }
--- routes/post-comment.js
+++ routes/post-comment.js
@@ -10,22 +10,13 @@ module.exports = (app, client) => {
     FROM users
     WHERE users.id = '${req.session.user.id}'`
   }
-  const query2 = {
-    text: `SELECT users.username, comments.comment, comments.message_id
-    FROM comments
-    LEFT JOIN users
-    ON users.id = comments.user_id`
-  }
 
   client.query(query, (err, result) => {
     if (err) throw err
-      client.query(query2, (err2, result2) => {
-        if (err2) throw err
-        var comments = result2.rows
-      })
 
-  res.render("all-messages", {comments:comments})
+  res.redirect("all-messages")
 
   })
 })
 }
+
